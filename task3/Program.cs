648b37c1885e6458052ca87686242c6ad06fb425
Anthelena <proffy_2010@mail.ru>
2022-12-15 01:42:56 +0300

    Задача 38

---
+++ task3/Program.cs
@@ -0,0 +1,40 @@
+﻿double[] array = randomArray(4);
+Console.WriteLine($"[{string.Join(", ",array)}] -> {(Math.Round(getMax(array) - getMin(array), 3))}");
+
+double[] randomArray(int n) 
+{
+    Random random = new Random();
+    double[] array = new double[n];
+    for(int i = 0; i < array.Length; i++) 
+    {
+        array[i] = Math.Round(random.NextDouble(), 3);
+    }
+    return array;
+}
+
+double getMin(double[] array) 
+
+{
+    double min = array[0];
+    for(int i = 1; i < array.Length; i++) 
+    {
+        if(array[i] < min) 
+        {
+            min = array[i];
+        }
+    }
+    return min;
+}
+
+double getMax(double[] array) 
+{
+    double max = array[0];
+    for(int i = 1; i < array.Length; i++) 
+    {
+        if(array[i] > max) 
+        {
+            max = array[i];
+        }
+    }
+    return max;
+}
